cff06541c9068c82f36ed46cd3281614da8828c2
[Luke Olende] <[olendeluke2k@gmail.com]>
2024-09-26 21:03:17 +0300

    add responsiveness

--- main.js
+++ main.js
@@ -51,7 +51,52 @@ let swiperFavorite = new Swiper(".favorite_swiper", {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav_menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true, //Animations Repeat
+});
+
+sr.reveal(`.home_social, .favorite_container, .sponsor_container, .footer`);
+sr.reveal(`.home_title span:nth-child(1)`, { origin: "left", opacity: 1 });
+sr.reveal(`.home_title span:nth-child(5)`, { origin: "right", opacity: 1 });
+sr.reveal(`.home_title tooltip, .home_button, .model_button`, {
+  origin: "botton",
+});
+sr.reveal(`.about_data`, { origin: "left" });
+sr.reveal(`.about_img, .model_tooltip`, { origin: "right" });
